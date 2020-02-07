ecaf4ad1e1b861c1a2fb576a02971e972d82e853
zakgy <ztcgy11@gmail.com>
2020-02-07 10:52:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/commplus/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.commplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClickSocial (View view) {
+        Intent intent = new Intent(this, Social.class);
+        startActivity(intent);
+    }
+}

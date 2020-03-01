98d7c77be7e5cbbf881465b91c26353f09eab56e
LKuramoto <kuramotolindsay@gmail.com>
2020-03-01 15:35:29 -0500

    Add project files.

---
+++ TreeHashTable/TreeHashTable.py
@@ -0,0 +1,71 @@
+import csv
+from collections import defaultdict
+MAX_CHILDREN = 6
+CSV_COLUMNS = ['Level', 'Parent', 'Child 1', 'Child 2',	'Child 3', 'Child 4', 'Child 5', 'Child 6']
+HASH_OUTPUT_COLUMNS = ['ID', 'Name']
+
+def CSVtoHash(newHash):
+
+    if newHash == True:
+        filename = 'Tree.csv'
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            hash = {}
+            i = 0
+            for row in reader:
+                hash[i] = row[CSV_COLUMNS[1]]
+                if row[CSV_COLUMNS[0]] == '4':
+                    childIds = getChildIds(i)
+                    for j in range(MAX_CHILDREN):
+                        hash[childIds[j]] = row[CSV_COLUMNS[j + 2]]
+                i = i + 1
+    else:
+        filename = 'Hash.csv'
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            hash = {}
+            for row in reader:
+                print(row)
+                hash[row[HASH_OUTPUT_COLUMNS[0]]] = row[HASH_OUTPUT_COLUMNS[1]]
+    return hash
+
+def HashtoCSV(filename, hash):
+    writer = csv.writer(open(filename, "w"))
+    writer.writerow(HASH_OUTPUT_COLUMNS)
+    for key, val in hash.items():
+        writer.writerow([key, val])
+    return
+
+def getChildIds(parentId):
+    childIds = [None] * MAX_CHILDREN # empty list
+    relativeChildId = 1 + parentId * MAX_CHILDREN
+    for i in range(MAX_CHILDREN):
+        childIds[i] = relativeChildId + i # offset
+    return childIds
+
+def getChildNames(parentId, hash):
+    names = [None] * MAX_CHILDREN # empty list
+    childIds = getChildIds(parentId)
+    for i in range(MAX_CHILDREN):
+        names[i] = hash[childIds[i]]
+    return names
+
+def getUniqueWords(hash):
+    words = {}
+    i = 0
+    for key, val in hash.items():
+        words[val] = "" # nothing
+    return words
+
+def main():
+
+    hash = CSVtoHash(1)
+    HashtoCSV("Hash.csv", hash)
+    words = getUniqueWords(hash)
+    HashtoCSV("Unique_Words.csv", words)
+    return
+
+if __name__ == '__main__':
+    main()
+
+print('one giant red bean')

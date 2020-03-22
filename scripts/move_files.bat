ea149c56aa5782a217d19d903ca35c376bdf0fc2
zakgy <ztcgy11@gmail.com>
2020-03-22 17:57:20 -0400

    Adding ImageParsing.csv file.

---
+++ scripts/move_files.bat
@@ -0,0 +1,5 @@
+del ..\app\src\main\res\layout\gen*
+copy layouts\* ..\app\src\main\res\layout\
+
+del ..\app\src\main\java\com\example\commplus\gen*
+copy java\* ..\app\src\main\java\com\example\commplus\
